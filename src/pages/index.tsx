3644461e71df1de26cdfd161db56f19b07b4b1c1
raizumaraiz <raizumaraiz@gmail.com>
2022-03-30 10:20:33 +0800

    Add support for subfolders

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,33 @@ import { SubTitle, Title } from "../styled/Shared"
 import { SubNote, ArticleEntryContainer, ArticleEntry, ArticleEntriesContainer, ArticleEntriesContainerHeadline } from "../styled/Components/ArticleList"
 
 const IndexPage = () => {
-  const posts: PostQueryData = useStaticQuery(graphql`
+  const files: AllFileData = useStaticQuery(graphql`
     query MyQuery {
-      allMdx(sort: {fields: frontmatter___last_modified_date, order: DESC}) {
+      allFile(
+        sort: {fields: childrenMdx___frontmatter___last_modified_date, order: DESC}
+        filter: {childrenMdx: {elemMatch: {frontmatter: {published: {eq: true}}}}}
+      ) {
         nodes {
-          frontmatter {
-            title
-            subtitle
-            written_date,
-            tags
+          id
+          childMdx {
+            id
+            slug
+            frontmatter {
+              title
+              subtitle
+              written_date
+              tags
+            }
           }
-          slug
+          ext
+          relativeDirectory
         }
+        totalCount
       }
-    }  
-  `);
+    }
+  `)
+
+  const mainTypes = _.uniqBy(files.allFile.nodes, 'relativeDirectory').map((file) => file.relativeDirectory);
 
   return (
     <Layout>
@@ -29,32 +41,51 @@ const IndexPage = () => {
       <SubTitle>Why is this here?</SubTitle>
       <p>This is an outlet of the person whoever is posting on this site. I might or might not be meant to be read by anyone, however if you're here already, you are very welcome.</p>
       <ArticleEntriesContainer>
-        <ArticleEntriesContainerHeadline>Posts</ArticleEntriesContainerHeadline>
-        { posts.allMdx.nodes.map((post) => {
-          return (
-            <ArticleEntryContainer>
-              <ArticleEntry><Link to={post.slug}>{post.frontmatter.title}</Link> - {post.frontmatter.subtitle}</ArticleEntry>
-              <SubNote>Written: {new Date(post.frontmatter.written_date).toLocaleDateString()}, Tags: { _.join(post.frontmatter.tags, ', ') }</SubNote>
-            </ArticleEntryContainer>
-            )
-        }) }
+        { 
+          mainTypes.map((postType) => 
+            (<>
+              <ArticleEntriesContainerHeadline>{_.capitalize(postType)}</ArticleEntriesContainerHeadline>
+              {
+                _.filter(files.allFile.nodes, (node) => node.relativeDirectory === postType).map((post) => {
+                  const { slug } = post.childMdx;
+                  const { written_date, title, subtitle, tags } = post.childMdx.frontmatter;
+                  const tagString = _.join(tags, ', ');
+
+                  return (
+                    <ArticleEntryContainer>
+                      <ArticleEntry><Link to={slug}>{title}</Link> - { subtitle }</ArticleEntry>
+                      <SubNote>Written: {new Date(written_date).toLocaleDateString()}, Tags: { tagString }</SubNote>
+                    </ArticleEntryContainer>
+                  )
+                })
+              }
+            </>)
+          )
+        }
       </ArticleEntriesContainer>
     </Layout>
   )
 }
 
-interface PostQueryData {
-  allMdx: {
+interface AllFileData {
+  allFile: {
     nodes: [{
-      frontmatter: {
-        title: string;
-        subtitle: string;
-        written_date: string;
-        tags: [string];
-        published: boolean;
-      }
-      slug: string
-    }]
+      id: string,
+      relativePath: string
+      childMdx: {
+        id: string,
+        slug: string,
+        frontmatter: {
+          title: string,
+          subtitle: string,
+          written_date: string,
+          tags: [string]
+        }
+      },
+      ext: string
+      relativeDirectory: string
+    }],
+    totalCount: number
   }
 }
 
