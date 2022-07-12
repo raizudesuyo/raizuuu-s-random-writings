1d29434cfa1e7413fa831e0f6095fc52d6df8fd2
raizumaraiz <raizumaraiz@gmail.com>
2022-07-12 15:09:48 +0800

    Added Google Adsense

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -14,6 +14,7 @@ import Header from "./header"
 import { GlobalStyle } from "../styled/Global";
 import { SmallNote } from "../styled/Shared";
 import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
+import AdsenseBanner from "./AdsenseBanner/AdsenseBanner";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,10 +26,21 @@ const Layout = ({ children }) => {
       }
     }
   `)
+
+  React.useEffect(() => {
+    try {
+      const adsbygoogle = window.adsbygoogle || []
+      adsbygoogle.push({})
+    } catch (e) {
+      console.error(e)
+    }
+  }, [])
+
   deckDeckGoHighlightElement();
 
   return (
     <>
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <GlobalFonts />
       <GlobalStyle />
       
@@ -52,6 +64,9 @@ const Layout = ({ children }) => {
         <footer>
           <SmallNote><a href="/">Back to Main Page</a></SmallNote>
         </footer>
+        <AdsenseBanner slot="bottom" style={{
+          display: 'inline-block';
+        }}/>
       </div>
     </>
   )
